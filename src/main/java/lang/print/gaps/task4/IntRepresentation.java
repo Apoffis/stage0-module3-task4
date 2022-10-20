8ecb4c69ab9b913bc7303dbe968ed13c23cacc29
Apoffis <abc22_08@mail.ru>
2022-10-20 15:31:04 +0400

    test4 commit 2

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -1,10 +1,16 @@
 package lang.print.gaps.task4;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class IntRepresentation {
+    private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
     public static void main(String[] args) {
-        System.out.println("99");
-        System.out.println("104");
-        System.out.println("97");
-        System.out.println("114");
+        LOGGER.log(Level.INFO, "99");
+        LOGGER.log(Level.INFO, "104");
+        LOGGER.log(Level.INFO, "97");
+        LOGGER.log(Level.INFO, "114");
     }
+
 }

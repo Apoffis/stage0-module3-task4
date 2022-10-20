ae8d0b5ddd6de5070552ff1ce7310291be9c4124
Apoffis <abc22_08@mail.ru>
2022-10-20 15:13:48 +0400

    test4 commit

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        float a = 4, b = 3;
+        double res;
+
+        res = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
     }
 }
